30f188dcd900a9e85a0e2a3924fa620ca27c57bc
github username <github email address>
2020-06-23 20:19:22 -0400

    added basic component setup and rendered index of restaurants

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+// import MainContainer from './containers/MainContainer'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import About from './components/About'
+import Signup from './components/Signup'
+import Login from './components/Login'
+// import MyReviews from './components/MyReviews'
+import Home from './components/Home'
+// import Restaurant from './components/Restaurant';
+import RestaurantContainer from './containers/RestaurantContainer'
+// import RestaurantForm from './components/RestaurantForm'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+
+          <h1 className="appname"> Foods We Like</h1>
+          <Navbar />
+          <Route exact path='/home' component={Home} />
+          {/* <Route exact path='/restaurant' component={Restaurant} /> */}
+          {/* need exact path in order for the about path to not render the above too */}
+          <Route path='/about' component={About} />
+          {/* {/* <Route path='/restaurants' component={Restaurants}/> */}
+       {/* <Route path= '/restaurants/:id' component={Restaurant}/> */} 
+          
+          {/* <Route path='/addrestaurant' component={RestaurantForm}/> */}
+          <Route path='/signup' component={Signup} />
+          <RestaurantContainer />
+          <Route path='/login' component={Login} />
+          {/* <Route path='/myreviews' component={MyReviews} /> */}
+
+
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+
+ import React, { Component } from 'react'
+ 
+ class About extends Component {
+     render() {
+         return (
+            <div className="about">
+            <h1> About Page </h1>
+                <div className="auth-form"> 
+                    <ul>
+                    <h5> Why Starts Foods we Like </h5>
+                   <p> <img alt="" src="https://cdn5.vectorstock.com/i/thumb-large/30/09/friends-eat-fast-food-and-spend-time-together-vector-31323009.jpg"  /> </p>
+                Two reasons. I am trying to practice my coding. Also, I am a big foodie. Thus, I wanted to create a more intimate platform where people could share their favorite places with their friends when in their home city or visiting others! 
+                    </ul> 
+                </div>
+            </div>
+         )
+     }
+ }//end
+
+export default About;
